d78ab7cbbff550bb934b7873f2dfd89493b5ec97
yamat47 <yamat47.thirddown@gmail.com>
2025-08-19 00:35:48 +0900

    feat: Word Counterの基本実装 - コントローラーとルーティング
    - WordCounterコントローラーを実装
    - showとcreateアクションを追加
    - RESTfulなルーティングを設定

---
+++ app/controllers/word_counter_controller.rb
@@ -0,0 +1,26 @@
+class WordCounterController < ApplicationController
+  def show
+    @text = ''
+    @word_count = 0
+    @character_count = 0
+  end
+
+  def create
+    @text = params[:text] || ''
+    @word_count = count_words(@text)
+    @character_count = @text.length
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { render :show }
+    end
+  end
+
+  private
+
+  def count_words(text)
+    return 0 if text.blank?
+
+    text.split(/\s+/).reject(&:blank?).count
+  end
+end
